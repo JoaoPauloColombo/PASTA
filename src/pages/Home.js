e1022931dc84c022a6ae28aede276acd0b445ee0
João Paulo <joaosouzapaulo446@gmail.com>
2024-06-05 16:23:48 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+// import Main from '../components/Main';
+// import Numeros from '../components/Numeros';
+// import Projetos from '../components/Projetos';
+// import Noticias from '../components/Noticias';
+// import Vagas from '../components/Vagas';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Background from "../assetsFinal/tropical-leaves.jpg";
